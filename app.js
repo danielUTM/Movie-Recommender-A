6918a4e6ea3e064bea69a197928bdc9c57cbdcff
daniel-campbell-327 <63069027+daniel-campbell-327@users.noreply.github.com>
2022-04-21 20:54:16 +0100

    Fixed database error when more than 8 films rated (res added to api call)

--- app.js
+++ app.js
@@ -293,6 +293,12 @@ app.post('/postUserBehaviour', function(req, res){
         $button: req.body.button,
         $timestamp: req.body.timestamp,
     })
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+     });
+    res.end(JSON.stringify({
+        status: 'success',
+    }));
 })
 
 //POST request to login
